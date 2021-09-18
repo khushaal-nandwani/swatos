b6f8d11f8ac4bac1ec7fc74442ae4c8c1b0b184a
Nguyen Tran <nguyentran1072e@gmail.com>
2021-09-18 15:42:43 -0400

    [not working] Tried to add a BoxLayout

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.label import Label
 
 class MainApp(App):
 
     def build(self):
-        return Label(text="Hack the North")
+        # I'm trying to make the boxes vertical.
+        options = BoxLayout(orientation="vertical")
+
+        # I'm trying to put some text in the boxes.
+        # Currently not working. Can anyone more experienced show me?
+        sublime = Label(text="sublime")
+        options.add_widget(sublime)
 
 
 if __name__ == '__main__':
